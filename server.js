7df31109ac20fe64bf73d8869b8e5d109c063da5
Parthik04-cyber <parthikchadotara@gmail.com>
2025-08-13 12:59:57 +0530

    added cors

--- server.js
+++ server.js
@@ -33,8 +33,9 @@ app.get("/", (req, res) => {
 });
 
 // INFO: Start server
-app.listen(port, () =>
-  console.log(`Server is running on at http://localhost:${port}`)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server is running on at http://localhost:${PORT}`)
 );
 
 const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
