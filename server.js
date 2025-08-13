b6099c11967da7cffcd9f2e2d26a90f035f69a08
Parthik04-cyber <parthikchadotara@gmail.com>
2025-08-13 13:11:24 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
 import dotenv from "dotenv";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
@@ -11,36 +10,34 @@ import orderRouter from "./routes/orderRoute.js";
 
 dotenv.config();
 
-// INFO: Create express app
 const app = express();
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
+
+// Connect to DB & Cloudinary
 connectDB();
 connectCloudinary();
 
-// INFO: Middleware
-app.use(express.json());
-app.use(cors());
-
-// INFO: API endpoints
-
-app.use("/api/user", userRouter);
-app.use("/api/product", productRouter);
-app.use("/api/order", orderRouter);
-
-// INFO: Default route
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-// INFO: Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server is running on at http://localhost:${PORT}`)
-);
-
+// CORS setup (before routes)
 const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
-
 app.use(cors({
   origin: allowedOrigins,
   credentials: true
 }));
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/order", orderRouter);
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
